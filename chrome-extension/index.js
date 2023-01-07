1ade0522c954ee09a9586113af00a6a416b9d1cf
ginoongflores <f.christianpaul@outlook.com>
2023-01-07 18:13:01 +0800

    inserted codes

---
+++ chrome-extension/index.js
@@ -0,0 +1,40 @@
+
+// const are cannot be reassigned to a new value
+
+// Methodology: If possible, use const. If not, use let. 
+
+let myLeads = []
+const inputBtn = document.querySelector('#input-btn')
+const inputEl = document.querySelector('#input-el')
+const ulEl = document.querySelector('#ul-el')
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+
+    // clear the input value after every input submission
+    inputEl.value = ""
+    renderLeads()
+
+})
+
+function renderLeads() {
+
+    // the listItems variable will hold all the HTML list items elements
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        console.log(listItems)
+
+
+
+    }
+
+    // Render the listItems inside the unordered list using ulEl.innerHTML
+    ulEl.innerHTML = listItems
+
+}
+
+
