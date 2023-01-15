83d68766ea9cef5186d47b0c20d8f7f96b1d6bf6
ginoongflores <florespaul161@gmail.com>
2023-01-15 21:18:13 +0800

    feat(input&tab): button functions

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -44,11 +44,33 @@ function renderLeads() {
 
 }
 
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+
+    // clear the input value after every input submission
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+})
+
+tabBtn.addEventListener("click", function () {
+
+    // get the link from the current tab
+    // here we have the tabs(object) with the tabs key which is also an object with a query method
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+})
+
+
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
-    renderLeads() // just call the renderLeads() as it will render an empty list
-
+    render(myLeads) // just call the render with myLeads as an argument to clear the list
 })
 
 
