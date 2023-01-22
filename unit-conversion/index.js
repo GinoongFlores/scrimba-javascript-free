d9c9c2dec80451bdcbe755df68f25d48ef38a6b5
ginoongflores <florespaul161@gmail.com>
2023-01-22 20:05:26 +0800

    add comments
    add comments for readability and for  future reference

--- unit-conversion/index.js
+++ unit-conversion/index.js
@@ -1,15 +1,21 @@
 const convertBtn = document.getElementById('btn-convert')
 const inputUnit = document.getElementById('unit-number')
 
-
+// set default width & height
 inputUnit.style.width = "117px"
 inputUnit.style.height = "98px"
 
 inputUnit.addEventListener("input", function () {
     const inputValue = inputUnit.value
 
+    // Check if the input value is greater than 0
+    // If true, set the width of the input field to the length of the input value multiplied by 117 pixels
+    // If false, set the width of the input field to 117 pixels
     inputUnit.style.width = inputValue.length > 0 ? (inputValue.length * 117) + "px" : "117px"
+
+    // check if the width of the input field is greater than 500 pixels
     if (inputUnit.clientWidth > 500) {
+        // if true, set the max-width of the input field to 500 pixels
         inputUnit.style.maxWidth = "500px"
     }
 })
